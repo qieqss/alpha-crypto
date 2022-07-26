df00a5ae87991c88da573f1884732e71f080f765
Alexey <kulakovalexey@proton.me>
2022-07-26 12:52:36 +0300

    added loading (homepage)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,31 @@ import React from "react";
 import Illustration from "../assets/movies.svg";
 import Nav from "../components/Nav";
 import { useNavigate } from "react-router-dom";
+import { Loop } from "@mui/icons-material";
+import axios from "axios";
 
 const Home = () => {
   const navigate = useNavigate();
   const [query, setQuery] = React.useState("");
+  const [loading, setLoading] = React.useState();
 
   function handleSearch() {
-    // (query !== null && query !== "")
-    query && (localStorage.setItem("searchValue", query), navigate("/browse"));
+    query && localStorage.setItem("searchValue", query);
+  }
+
+  function fetchMovies(query) {
+    setLoading(true);
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&api_key=b69c1933d70772560f256dfcc45c6056&query=${query}&language=en-US`
+      )
+      .then(() => {
+        setLoading(false);
+        navigate("/browse");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   return (
@@ -39,16 +56,22 @@ const Home = () => {
                     value={query}
                     onChange={(event) => setQuery(event.target.value)}
                     onKeyDown={(event) =>
-                      event.key === "Enter" && handleSearch()
+                      event.key === "Enter" && (handleSearch(), fetchMovies())
                     }
                   />
                 </div>
-                <IconButton
-                  className="search__img--wrapper"
-                  onClick={handleSearch}
-                >
-                  <Search fontSize="large" />
-                </IconButton>
+                {loading ? (
+                  <IconButton className="search__img--wrapper">
+                    <Loop fontSize="large" className="loading__icon" />
+                  </IconButton>
+                ) : (
+                  <IconButton
+                    className="search__img--wrapper"
+                    onClick={(handleSearch, fetchMovies)}
+                  >
+                    <Search fontSize="large" />
+                  </IconButton>
+                )}
               </div>
               <figure>
                 <img src={Illustration} alt="" />
